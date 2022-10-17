75429ff514003843a3899ff33c17d2ed6319c300
Liang.cui <cuiliang0302@qq.com>
2022-10-17 15:53:27 +0800

    优化store结构

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -22,3 +22,21 @@ export function onloadTheme() {
   console.log("首次加载了啊")
   setTheme(getThemeValue(themeName))
 }
+// export default function useTheme() {
+//   const theme = useThemeStore()
+//   // 获取当前主题色
+//   const {themeName} = storeToRefs(theme)
+//
+//   // 切换主题色
+//   function setTheme(value) {
+//     console.log("theme.js", value)
+//     const el = ref(null)
+//     const primary_color = useCssVar('--el-color-primary', el)
+//     primary_color.value = getThemeValue(value)
+//     return null
+//   }
+//   onMounted(()=>{
+//     console.log("哈哈哈，只想了")
+//   })
+//   return {themeName, setTheme}
+// }
